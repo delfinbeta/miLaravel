de2d5278729722fb71161f6acf489104f7524cf9
Dayan Betancourt <dkbetancourt@gmail.com>
2018-11-04 22:45:30 -0400

    Agregando Rutas

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,22 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+  return view('welcome');
+});
+
+Route::get('/usuarios', function () {
+  return "Usuarios";
+});
+
+Route::get('/usuarios/nuevo', function () {
+  return "Crear Nuevo Usuario";
+});
+
+Route::get('/usuarios/{id}', function ($id) {
+  return "Mostrando detalle del usuario: {$id}";
+})->where(['id' => '[\d]+']);
+
+Route::get('/saludo/{nombre}/{apodo?}', function ($nombre, $apodo = null) {
+  if($apodo) { return "Bienvenido {$nombre}, tu apodo es {$apodo}"; }
+  else { return "Bienvenido {$nombre}, sin apodo"; }
 });

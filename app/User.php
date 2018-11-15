f319fa12f6415378a9821e872ef3f002e87ca3ab
Dayan Betancourt <dkbetancourt@gmail.com>
2018-11-14 20:19:44 -0400

    Atributos en Eloquent

--- app/User.php
+++ app/User.php
@@ -28,6 +28,10 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
+    protected $casts = [
+        'is_admin' => 'boolean'
+    ];
+
     public function isAdmin() {
         return $this->email === 'dkbetancourt@gmail.com';
     }

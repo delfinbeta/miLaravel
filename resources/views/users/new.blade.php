77796ef8e74300e76d75f387eb3938c2e3f85786
Dayan Betancourt <dkbetancourt@gmail.com>
2018-11-20 22:41:25 -0400

    Crear Formulario

--- resources/views/users/new.blade.php
+++ resources/views/users/new.blade.php
@@ -7,6 +7,20 @@
 	<hr />
 	<form method="POST" action="{{ url('usuarios/nuevo') }}">
 		{{ csrf_field() }}
+
+		<div class="form-group">
+			<label for="name">Nombre:</label>
+			<input type="text" name="name" id="name" class="form-control">
+		</div>
+		<div class="form-group">
+			<label for="email">Email:</label>
+			<input type="email" name="email" id="email" class="form-control">
+		</div>
+		<div class="form-group">
+			<label for="password">Contraseña:</label>
+			<input type="password" name="password" id="password" class="form-control">
+		</div>
+
 		<button type="submit" class="btn btn-info">Crear Usuario</button>
 	</form>
 @endsection

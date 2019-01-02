241fabd8915702a26a5daa296fd2b9a43a53b3af
Dayan Betancourt <dkbetancourt@gmail.com>
2019-01-02 18:17:07 -0400

    Laravel Dusk Incompleto :(

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    miLaravel - Curso Styde.net
+                    mi Laravel - Curso Styde.net
                 </div>
 
                 <div class="links">
